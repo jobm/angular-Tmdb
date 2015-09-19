9fd94eeec5ce29c6a288a8e06b4c7e906ab94cf7
JobM <jobmwaniki18@gmail.com>
2015-09-19 12:06:44 +0300

    images are working better

--- app/app.js
+++ app/app.js
@@ -1,12 +1,13 @@
 var app = angular.module("MovieApp", ['ngRoute']);
 
 app.config(function($routeProvider){
-  $routeProvider
-  .when('/',{
-    templateUrl:'pages/movies.html'
-    controller:'mainController'
-  })
-}
+    $routeProvider
+    .when('/',{
+        templateUrl:'pages/movies.html',
+        controller: 'mainController'
+    })
+});
+
 app.controller("mainController", function($scope,$http) {
 
   $http.get("https://api.themoviedb.org/3/movie/top_rated?api_key=b50218f262d316468a8928b0f29ac476").success(function(response){
