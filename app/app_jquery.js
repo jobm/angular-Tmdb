67e22cda7cf68fbfb1c0dd90acb28acc84bb1b9a
JobM <jobmwaniki18@gmail.com>
2015-09-19 20:28:09 +0300

    removed jquer file

--- app/app_jquery.js
+++ app/app_jquery.js
@@ -1,7 +1,6 @@
 $(function(){
-    $('.over_view').ready()(function(){
-     over_view = $('.over-view').text();
-     alert(orver_view);
-    });
+    if(!$('.over-view').is(':empty')){
+        alert($('.over-view').text());
+    }
 });
 
