56faa2b3dc2535f6b90b66752622ea08746e296d
JobM <jobmwaniki18@gmail.com>
2015-09-19 17:42:01 +0300

    movie params working

--- app/app.js
+++ app/app.js
@@ -1,11 +1,26 @@
 var app = angular.module('app', ['ngRoute']);
 
+app.config(function($routeProvider){
+    $routeProvider
+    .when('/',{
+        templateUrl:'pages/main.html',
+        controller: 'mainController'
+    })
+
+    .when('/movies/:prm',{
+        templateUrl:'pages/movies.html',
+        controller: 'moviesController'
+    })
+});
+
+
 app.controller("mainController", function($scope,$http,$routeParams) {
  $scope.title = "Movies";
 });
 
-app.controller("moviesCotroller", function($scope,$http,$routeParams) {
-    $http.get("https://api.themoviedb.org/3/movie/top_rated?api_key=b50218f262d316468a8928b0f29ac476").success(function(response){
+app.controller("moviesController", function($scope,$http,$routeParams) {
+    $scope.prm = $routeParams.prm;
+    $http.get("https://api.themoviedb.org/3/movie/"+$scope.prm+"?api_key=b50218f262d316468a8928b0f29ac476").success(function(response){
     $scope.movies = response.results;
     $scope.image_url = response.results.poster_path;
   });
@@ -21,18 +36,6 @@ app.controller("moviesCotroller", function($scope,$http,$routeParams) {
 //    };
 //});
 
-app.config(function($routeProvider){
-    $routeProvider
-    .when('/',{
-        templateUrl:'pages/main.html',
-        controller: 'mainController'
-    })
-
-    .when('/movies',{
-        templateUrl:'pages/movies.html',
-        controller: 'moviesCotroller'
-    })
-});
 
 
 
