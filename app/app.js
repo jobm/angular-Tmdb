58c986f1eafac1e8705547e47894d201ddf8a771
JobM <jobmwaniki18@gmail.com>
2015-09-19 15:45:48 +0300

    created movies controller

--- app/app.js
+++ app/app.js
@@ -1,28 +1,41 @@
-var app = angular.module("MovieApp", ['ngRoute']);
-
-app.config(function($routeProvider){
-    $routeProvider
-    .when('/',{
-        templateUrl:'pages/movies.html',
-        controller: 'mainController'
-    })
-});
+var app = angular.module('app', ['ngRoute']);
 
 app.controller("mainController", function($scope,$http,$routeParams) {
+ $scope.title = "Movies";
+});
 
-  $http.get("https://api.themoviedb.org/3/movie/top_rated?api_key=b50218f262d316468a8928b0f29ac476").success(function(response){
+app.controller("moviesCotroller", function($scope,$http,$routeParams) {
+    $http.get("https://api.themoviedb.org/3/movie/top_rated?api_key=b50218f262d316468a8928b0f29ac476").success(function(response){
     $scope.movies = response.results;
     $scope.image_url = response.results.poster_path;
   });
 });
+//app.directive('movieImage', function(){
+//    return function(scope, element, attrs) {
+//        scope.$watch(attrs.movieImage, function(value) {
+//            element.css({
+//                'background-image': 'url(' + value +')',
+//                'background-size' : 'cover'
+//            });
+//        });
+//    };
+//});
 
-app.directive('moviePoster', function(){
-    return function(scope, element, attrs){
-        attrs.$observe('moviePoster', function(value) {
-            element.css({
-                'background-image': 'url(' + value +')',
-                'background-size' : 'cover'
-            });
-        });
-    };
+app.config(function($routeProvider){
+    $routeProvider
+    .when('/',{
+        templateUrl:'pages/main.html',
+        controller: 'mainController'
+    })
+
+    .when('/movies',{
+        templateUrl:'pages/movies.html',
+        controller: 'moviesCotroller'
+    })
 });
+
+
+
+
+
+
