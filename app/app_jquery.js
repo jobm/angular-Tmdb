fd43592e2e09a2198a8cbb2b06a51d9e630b2824
JobM <jobmwaniki18@gmail.com>
2015-09-20 12:54:35 +0300

    jquery class active working

--- app/app_jquery.js
+++ app/app_jquery.js
@@ -1,7 +1,27 @@
 $(function(){
     $('#tab1').click(function(){
-        alert('')'
-        $('#tab1').removeClass(".is-active");
+        $('#tab1').addClass("is-active-tab");
+        $('#tab2').removeClass("is-active-tab");
+        $('#tab3').removeClass("is-active-tab");
+        $('#tab4').removeClass("is-active-tab");
+    });
+      $('#tab2').click(function(){
+        $('#tab2').addClass("is-active-tab");
+        $('#tab1').removeClass("is-active-tab");
+        $('#tab3').removeClass("is-active-tab");
+        $('#tab4').removeClass("is-active-tab");
+    });
+      $('#tab3').click(function(){
+        $('#tab3').addClass("is-active-tab");
+        $('#tab1').removeClass("is-active-tab");
+        $('#tab2').removeClass("is-active-tab");
+        $('#tab4').removeClass("is-active-tab");
+    });
+      $('#tab4').click(function(){
+        $('#tab4').addClass("is-active-tab");
+        $('#tab1').removeClass("is-active-tab");
+        $('#tab2').removeClass("is-active-tab");
+        $('#tab3').removeClass("is-active-tab");
     });
 });
 
