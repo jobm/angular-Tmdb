4fb8eeb9f6d4e5b6be88544aba0a0acb8d825d41
JobM <jobmwaniki18@gmail.com>
2015-09-25 10:57:34 +0300

    added more content

--- app/app.js
+++ app/app.js
@@ -1,55 +1,63 @@
-var app = angular.module('app', ['ngRoute']);
+﻿var app = angular.module('app', ['ngRoute']);
 
-app.config(function($routeProvider){
+app.config(function ($routeProvider) {
     $routeProvider
-    .when('/',{
-        templateUrl:'pages/main.html',
+    .when('/', {
+        templateUrl: 'pages/main.html',
         controller: 'mainController'
     })
 
-    .when('/movies/:prm',{
-        templateUrl:'pages/movies.html',
+    .when('/movies/:prm', {
+        templateUrl: 'pages/movies.html',
         controller: 'moviesController'
     })
 
-    .when('/movie/details/:Id',{
+    .when('/movie/details/:Id', {
         templateUrl: 'pages/movie_detials.html',
         controller: 'moviesDetailsController'
     })
 
-    .when('/about',{
-        templateUrl:'pages/about.html',
-        controller:'mainController'
+    .when('/about', {
+        templateUrl: 'pages/about.html',
+        controller: 'mainController'
     })
 });
 
 
-app.controller("mainController", function($scope,$http,$routeParams) {
+app.controller("mainController", function ($scope, $http, $routeParams) {
 
 
-    $http.get("https://api.themoviedb.org/3/movie/now_playing?api_key=b50218f262d316468a8928b0f29ac476").success(function(response){
+    $http.get("https://api.themoviedb.org/3/movie/now_playing?api_key=b50218f262d316468a8928b0f29ac476").success(function (response) {
 
         $scope.first_movie = response.results[0];
         $scope.sec_movie = response.results[1];
         $scope.third_movie = response.results[2];
         $scope.fourth_movie = response.results[3];
 
-  });
+    });
 });
 
-app.controller("moviesController", function($scope,$http,$routeParams) {
+app.controller("moviesController", function ($scope, $http, $routeParams) {
     $scope.prm = $routeParams.prm;
-    $http.get("https://api.themoviedb.org/3/movie/"+$scope.prm+"?api_key=b50218f262d316468a8928b0f29ac476").success(function(response){
-    $scope.movies = response.results;
-//    $scope.image_url = response.results.poster_path;
+    $http.get("https://api.themoviedb.org/3/movie/" + $scope.prm + "?api_key=b50218f262d316468a8928b0f29ac476").success(function (response) {
+        $scope.movies = response.results;
+        //    $scope.image_url = response.results.poster_path;
 
-  });
+    });
 });
 
-app.controller("moviesDetailsController",function($scope,$http,$routeParams){
+app.controller("moviesDetailsController", function ($scope, $http, $routeParams) {
     $scope.id = $routeParams.Id;
-    $http.get("https://api.themoviedb.org/3/movie/"+$scope.id+"?api_key=b50218f262d316468a8928b0f29ac476").success(function(response){
+    $http.get("https://api.themoviedb.org/3/movie/" + $scope.id + "?api_key=b50218f262d316468a8928b0f29ac476").success(function (response) {
         $scope.movie = response;
+    });
+    //get backdrops
+    $http.get("https://api.themoviedb.org/3/movie/" + $scope.id + "/images?api_key=b50218f262d316468a8928b0f29ac476").success(function (response) {
+        $scope.back_drops = response.backdrops;
+    });
+    //get similar
+    $http.get("https://api.themoviedb.org/3/movie/" + $scope.id + "/similar?api_key=b50218f262d316468a8928b0f29ac476").success(function (response) {
+        $scope.similar = response.results;
     });
 });
 
--- app/app_jquery.js
+++ app/app_jquery.js
@@ -1,48 +1,56 @@
-$(function(){
-    $('.mdl-tabs__tab-bar > a').each(function(){
-        $(this).click(function(){
+﻿$(function () {
+    if (window.location.href !== "#/movies/popular" ||
+        window.location.href !== "#/movies/now_playing" ||
+        window.location.href !== "#/movies/upcoming" ||
+        window.location.href !== "#/movies/top_rated") {
+        $("mdl-tabs__tab-bar").each(function () {
+            $(this).removeClass('is-active-tab');
+        });
+    }
+    $('.mdl-tabs__tab-bar > a').each(function () {
+        $(this).click(function () {
             $('#home > span').removeClass('is-active-tab');
             $('#about > span').removeClass('is-active-tab')
         });
     });
     //$('.mdl-tabs__tab-bar > a')
-    $('#home').click(function(){
-        $('.mdl-tabs__tab-bar > a').each(function(){$(this).removeClass('is-active-tab')});
+    $('#home').click(function () {
+        $('.mdl-tabs__tab-bar > a').each(function () { $(this).removeClass('is-active-tab') });
         $('#home > span').addClass('is-active-tab');
         $('#about > span').removeClass('is-active-tab');
     });
-    $('#about').click(function(){
-        $('.mdl-tabs__tab-bar > a').each(function(){$(this).removeClass('is-active-tab')});
-         $('#about > span').addClass('is-active-tab');
+    $('#about').click(function () {
+        $('.mdl-tabs__tab-bar > a').each(function () { $(this).removeClass('is-active-tab') });
+        $('#about > span').addClass('is-active-tab');
         $('#home > span').removeClass('is-active-tab');
     });
-    $('#tab1').click(function(){
+    $('#tab1').click(function () {
         $('#tab1').addClass("is-active-tab");
         $('#tab2').removeClass("is-active-tab");
         $('#tab3').removeClass("is-active-tab");
         $('#tab4').removeClass("is-active-tab");
         window.location.href = "#/movies/popular";
     });
-      $('#tab2').click(function(){
+    $('#tab2').click(function () {
         $('#tab2').addClass("is-active-tab");
         $('#tab1').removeClass("is-active-tab");
         $('#tab3').removeClass("is-active-tab");
         $('#tab4').removeClass("is-active-tab");
         window.location.href = "#/movies/now_playing";
     });
-      $('#tab3').click(function(){
+    $('#tab3').click(function () {
         $('#tab3').addClass("is-active-tab");
         $('#tab1').removeClass("is-active-tab");
         $('#tab2').removeClass("is-active-tab");
         $('#tab4').removeClass("is-active-tab");
         window.location.href = "#/movies/upcoming";
     });
-      $('#tab4').click(function(){
+    $('#tab4').click(function () {
         $('#tab4').addClass("is-active-tab");
         $('#tab1').removeClass("is-active-tab");
         $('#tab2').removeClass("is-active-tab");
         $('#tab3').removeClass("is-active-tab");
-         window.location.href ="#/movies/top_rated"
+        window.location.href = "#/movies/top_rated"
     });
 });
 
